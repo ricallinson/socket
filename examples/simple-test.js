fc11586221f9338216cef2e0cd09ef24f7a81ecb
Ric Allinson <capecodehq@yahoo.co.uk>
2012-07-13 12:22:37 -0700

    JSLint

--- examples/simple-test.js
+++ examples/simple-test.js
@@ -1,58 +1,76 @@
+//    (The MIT License)
+//
+//    Copyright (c) 2012 Richard S Allinson <rsa@mountainmansoftware.com>
+//
+//    Permission is hereby granted, free of charge, to any person obtaining
+//    a copy of this software and associated documentation files (the
+//    'Software'), to deal in the Software without restriction, including
+//    without limitation the rights to use, copy, modify, merge, publish,
+//    distribute, sublicense, and/or sell copies of the Software, and to
+//    permit persons to whom the Software is furnished to do so, subject to
+//    the following conditions:
+//
+//    The above copyright notice and this permission notice shall be
+//    included in all copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+//    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-/*!
- * Knot
- * Copyright(c) 2012 RS Allinson
- * MIT Licensed
- */
+'use strict';
 
-var knot = require('knot'),
+var socket = require('../lib'),
     net = require('net'),
     microtime = require('microtime'),
     port = 8080,
-    app;
+    app,
+    repeat = 1000,
+    count = repeat,
+    start = microtime.now(),
+    i;
 
-app = knot.createServer(
-    knot.profiler(),
-    knot.echo()
+app = socket.createServer(
+    socket.profiler(),
+    socket.echo()
 ).listen(port);
 
 console.log('Knot Server Started');
 
-function test (port, ip, obj, callback){
-    var buffer = '';
-    var client = net.connect(port, ip, function() {
-        client.write(JSON.stringify(obj)+app.TERM);
+function test(port, ip, obj, callback) {
+    var buffer = '',
+        client;
+
+    client = net.connect(port, ip, function () {
+        client.write(JSON.stringify(obj) + app.TERM);
     });
-    client.on('data', function(data) {
-        buffer+= data;
+    client.on('data', function (data) {
+        buffer += data;
     });
-    client.on('end', function() {
+    client.on('end', function () {
         var data;
-        try{
+        try {
             data = JSON.parse(buffer);
-        }catch(e){
-            data = {error:'bad json parse'};
+        } catch (e) {
+            data = {error: 'bad json parse'};
         }
         callback(data);
     });
 }
 
-var repeat = 1000,
-    count = repeat,
-    start = microtime.now(),
-    i;
-
-for(i=0;i<count;i++){
-    test(port, 'localhost', {num:i}, function(msg){
-        log(msg);
-    });
-}
-
-function log (msg){
-    if(!--count){
-        console.log('Total: '+((microtime.now()-start)/1000000)+'sec ('+repeat+' requests)');
+function log(msg) {
+    count = count - 1;
+    if (!count) {
+        console.log('Total: ' + ((microtime.now() - start) / 1000000) + 'sec (' + repeat + ' requests)');
         process.exit();
-    }else{
+    } else {
         console.log(msg);
     }
+}
+
+for (i = 0; i < count; i = i + 1) {
+    test(port, 'localhost', {num: i}, log);
 }
